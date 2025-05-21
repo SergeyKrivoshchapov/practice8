5fbb5bbfcb905936e237b4769ee28ee399a12cd5
kserg <krivoshchapovsergey@gmail.com>
2025-05-21 13:05:48 +0700

    init 2

---
+++ Smartphone.cpp
@@ -0,0 +1,69 @@
+#include "Smartphone.h"
+Smartphone::Smartphone() {
+    this->model = ""; this->price = 0;
+    this->manufacturer = Manufacturer::UNDEFINED;
+    this->color = Color::UNDEFINED;
+    this->displaySize = 0; this->ram = 0;
+    this->storage = 0;
+    this->cpu = CPU::UNDEFINED; this->os = OS::UNDEFINED;
+}
+Smartphone::Smartphone(string model, double price, Manufacturer manufacturer,
+Color color, float displaySize, int ram, int storage, CPU cpu, OS os) {
+    this->model = model;
+    this->price = price;
+    this->manufacturer = manufacturer;
+    this->color = color;
+    this->displaySize = displaySize;
+    this->ram = ram;
+    this->storage = storage;
+    this->cpu = cpu;
+    this->os = os;
+}
+string Smartphone::getModel() const { return model; }
+Manufacturer Smartphone::getManufacturer() const { return manufacturer; }
+Color Smartphone::getColor() const { return color; }
+float Smartphone::getDisplay() const { return displaySize; }
+int Smartphone::getRAM() const { return ram; }
+int Smartphone::getStorage() const { return storage; }
+CPU Smartphone::getCPU() const { return cpu; }
+OS Smartphone::getOS() const { return os; }
+double Smartphone::getPrice() const { return price; }
+void Smartphone::setPrice(double price) { this->price = price; }
+
+string Smartphone::colorToString(Color c) {
+    switch (c) {
+        case Color::WHITE: return "WHITE";
+        case Color::GRAY: return "GRAY";
+        case Color::BLACK: return "BLACK";
+        case Color::BLUE: return "BLUE";
+        case Color::RED: return "RED";
+        case Color::GOLD: return "GOLD";
+        default: return "UNDEFINED";
+    }
+}
+
+string Smartphone::manufacturerToString(Manufacturer m) {
+    switch (m) {
+        case Manufacturer::APPLE: return "APPLE";
+        case Manufacturer::SAMSUNG: return "SAMSUNG";
+        case Manufacturer::XIAOMI: return "XIAOMI";
+        case Manufacturer::HTC: return "HTC";
+        default: return "UNDEFINED";
+    }
+}
+
+string Smartphone::osToString(OS o) {
+    switch (o) {
+        case OS::ANDROID: return "ANDROID";
+        case OS::IOS: return "IOS";
+        default: return "UNDEFINED";
+    }
+}
+
+string Smartphone::cpuToString(CPU cpu) {
+    switch (cpu) {
+        case CPU::SNAPDRAGON: return "SNAPDRAGON";
+        case CPU::APPLE: return "APPLE";
+        default: return "UNDEFINED";
+    }
+}
